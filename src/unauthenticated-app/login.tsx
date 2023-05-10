01f77b538c8ae101e7d5d3c2e81de76736a7e132
Shellyzheng27 <shelly.zheng27@gmail.com>
2023-05-10 03:05:39 -0400

    style: install emotion, optimize sign in and sign up page

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 import React, { FormEvent } from "react";
 import { Button, Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app";
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -21,7 +22,7 @@ export const LoginScreen = () => {
             <Input placeholder={"password"} type="password" id={"password"} />
         </Form.Item>
         <Form.Item>
-            <Button htmlType={'submit'} type={"primary"}>log in</Button>
+            <LongButton htmlType={'submit'} type={"primary"}>Sign in</LongButton>
         </Form.Item>
         
     </Form>
